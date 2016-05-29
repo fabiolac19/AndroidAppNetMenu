baee536e2488ffbe9b5c55064277cc14cca9f58d
Fabiola <fabiolac19@gmail.com>
2016-05-29 18:41:14 -0300

    Andando json en listView con MenuDraw

--- app/src/main/java/com/example/android/menunetlog/HomeFragment.java
+++ app/src/main/java/com/example/android/menunetlog/HomeFragment.java
@@ -1,24 +1,146 @@
 package com.example.android.menunetlog;
-
-import android.app.Fragment;
+import android.app.ListFragment;
+import android.content.ClipData;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by fabiola on 29/05/16.
  */
-public class HomeFragment extends Fragment {
+public class HomeFragment extends ListFragment {
+    private static final String STATE_COUNTER = "counter";
+    private int mCounter;
+    private static final String STATE_ITEMS = "items";
 
-    public HomeFragment(){}
+    // Make sure to declare as ArrayList so it's Serializable
+    private ArrayList<ClipData.Item> mItems;
 
+    private JSONArray jsonArray;
+    List<String> values;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        values = new ArrayList<>();
+        values.add(0,"pepe");
+
+        String url = "http://api.androidhive.info/contacts/";
+        json(url,"contacts");
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
+                R.layout.support_simple_spinner_dropdown_item, values);
+        setListAdapter(adapter);
+        if (savedInstanceState != null) {
+            mCounter = savedInstanceState.getInt(STATE_COUNTER, 0);
+        }
+    }
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+                ((MainActivityMenu) getActivity()).setActionBarTitle("Gestionar Estacion");
+        return view;
+    }
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+//        values = new ArrayList<>();
+//        values.add(0,"pepe");
+//
+//        String url = "http://api.androidhive.info/contacts/";
+//        json(url,"contacts");
+//        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
+//                R.layout.support_simple_spinner_dropdown_item, values);
+//        setListAdapter(adapter);
+    }
+    @Override
+    public void onViewStateRestored(Bundle savedInstanceState) {
+        super.onViewStateRestored(savedInstanceState);
+    }
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        values = new ArrayList<>();
+        values.add(0,"pepe");
 
-        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+        String url = "http://api.androidhive.info/contacts/";
+        json(url,"contacts");
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
+                R.layout.support_simple_spinner_dropdown_item, values);
+        setListAdapter(adapter);
+        outState.putInt(STATE_COUNTER, mCounter);
+        outState.putSerializable(STATE_ITEMS, mItems);
+    }
+    private void json(String url, final String nombre)
+    {
+        RequestQueue queue = Volley.newRequestQueue(getActivity());
 
-        return rootView;
+        JsonObjectRequest jsObjRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        // TODO Auto-generated method stub
+
+                        try {
+                            //extracting json array from response string
+                            jsonArray = response.getJSONArray(nombre);
+
+                            leerJson();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO Auto-generated method stub-
+
+                    }
+                });
+        queue.add(jsObjRequest);
+
+    }
+
+    private void leerJson()
+    {
+        String[] contacs = {
+                "id",
+                "name",
+                "email",
+                "address"
+        };
+        for (int i=0; i< jsonArray.length(); i++){
+            JSONObject jsonRow = null;
+            try {
+                jsonRow = jsonArray.getJSONObject(i);
+                for (int j = 0; j < contacs.length; j++ ){
+                    String resultStr = null;
+                    try {
+                        resultStr = jsonRow.getString(contacs[j]);
+                        if (j ==  1) {values.add(i,resultStr);}
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
+
--- app/src/main/java/com/example/android/menunetlog/MainActivityMenu.java
+++ app/src/main/java/com/example/android/menunetlog/MainActivityMenu.java
@@ -3,16 +3,13 @@ package com.example.android.menunetlog;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.util.Log;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -25,15 +22,6 @@ public class MainActivityMenu extends AppCompatActivity
         setContentView(R.layout.activity_main_activity_menu);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -54,27 +42,13 @@ public class MainActivityMenu extends AppCompatActivity
             super.onBackPressed();
         }
     }
-
+    public void setActionBarTitle(String title) {
+        getSupportActionBar().setTitle(title);
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main_activity_menu, menu);
         return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @SuppressWarnings("StatementWithEmptyBody")
@@ -84,18 +58,15 @@ public class MainActivityMenu extends AppCompatActivity
         int id = item.getItemId();
         Fragment fragment = null;
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_gestion) {
             fragment = new HomeFragment();
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_entradas) {
             fragment = new PagesFragment();
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_servidor) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_delay) {
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_calibrar) {
 
         }
         if (fragment != null) {
@@ -111,3 +82,4 @@ public class MainActivityMenu extends AppCompatActivity
         return true;
     }
 }
+
